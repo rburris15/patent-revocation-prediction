e7d149622970f003daa8e1fb65f895587ed42ef7
rburrisMDA <147848998+rburrisMDA@users.noreply.github.com>
2025-03-28 20:12:37 -0700

    clean up repo

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="your_project",
+    name="patent-revocation-prediction",
     version="0.1",
     packages=find_packages(where='src'),
     package_dir={"": "src"},
